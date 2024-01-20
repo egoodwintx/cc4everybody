a1fb837d85e5cfb7b470a21f1a8df2c52b5f013f
Ed Goodwin <ed.registration@gmail.com>
2024-01-19 23:33:37 -0600

    more exercises

---
+++ src/hoti_func.c
@@ -0,0 +1,36 @@
+//---------------------------------------------------------
+// file: htoi_use.c
+// project: Learning C
+// author: Ed Goodwin
+// date: 01.19.2024
+// description:
+//  simple program that includes htoi function
+//  that converts from hex to int
+//---------------------------------------------------------
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(void) {
+
+
+}
+
+int htoi(s)
+char s[];
+{
+    const char a = 10;
+    const char b = 11;
+    const char c = 12;
+    const char d = 13;
+    const char e = 14;
+    const char f = 15;
+    
+  int i, n;
+  n = 0;
+  while (s[i] != '\0'){
+    
+    i++;
+  }
+    	
+    
+}
