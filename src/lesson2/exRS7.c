ad3f293d047e38c1d3f53213c147a8f76e72f4fc
Ed Goodwin <ed.registration@gmail.com>
2024-07-07 00:17:20 -0500

    minor edit

--- src/lesson2/exRS7.c
+++ src/lesson2/exRS7.c
@@ -14,18 +14,16 @@ int main (void) {
     char line[1000];
     int minval = NULL;
     int maxval = NULL;
-  
-    fgets(line, 1000, stdin);
+    int i = 1;
+    
 
-    while (*line != "done" || *line != EOF) {
-       int i = atoi(line);
+    while (scanf("%d", &i) != EOF) {
        if (maxval == NULL || i > maxval){
            maxval = i;
        }
        if (minval == NULL || i < minval){
             minval = i;    
        }
-       fgets(line, 1000, stdin);
     }
     printf("Maximum %d\n", maxval);
     printf("Minimum %d\n", minval);
