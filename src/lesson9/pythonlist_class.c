952f35110f4a89a912d4035d4f4887c3e537a535
Ed Goodwin <ed.registration@gmail.com>
2024-07-25 07:32:04 -0500

    pylist code - currently segfaulting

--- src/lesson9/pythonlist_class.c
+++ src/lesson9/pythonlist_class.c
@@ -10,9 +10,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct lnode {
+    char *text;
+    struct lnode *next;
+};
+
+struct pylist {
+  struct lnode *head;
+  struct lnode *tail;
+  int count;
+};
+
 /* print(lst) */
 void pylist_print(struct pylist* self)
 {
+    struct lnode *cur, *next;
+    cur = self->head;
+    printf("[%s", cur->text);
+    cur = cur->next;
+    while(cur) {
+        printf(",'%s'", cur->text);
+        cur = cur->next;
+    }
+    printf("]\n");
     /* About 10 lines of code 
        The output should match Python's
        list output
@@ -28,30 +48,36 @@ void pylist_print(struct pylist* self)
 int pylist_len(const struct pylist* self)
 {
     /* One line of code */
-    return 42;
+    return self->count;
 }
 
 /* lst.append("Hello world") */
 void pylist_append(struct pylist* self, char *str) {
     /* Review: Chapter 6 lectures and assignments */
+    struct lnode* newtail = malloc(sizeof(*newtail));
+    newtail->next = NULL;
+    newtail->text = str;
+
+    (self->tail)->next = newtail;
+
 }
 /* lst.index("Hello world") - if not found -1 */
 int pylist_index(struct pylist* self, char *str)
 {
+    struct lnode* cur = self->head;
+    int counter = 0;
+    while (cur) {
+        counter++;
+        if (strcmp(cur->text, str)) {
+            return counter;
+        }
+        else {
+            cur = cur->next;
+        }
+    }
     /* Seven or so lines of code */
-    return 42;
+    return -1;
 }
-
-struct lnode {
-    char *text;
-    struct lnode *next;
-};
-
-struct pylist {
-  struct lnode *head;
-  struct lnode *tail;
-  int count;
-};
 
 /* Constructor - lst = list() */
 struct pylist * pylist_new() {
@@ -82,12 +108,12 @@ int main(void)
     struct pylist * lst = pylist_new();
     pylist_append(lst, "Hello world");
     pylist_print(lst);
-    pylist_append(lst, "Catch phrase");
-    pylist_print(lst);
-    pylist_append(lst, "Brian");
-    pylist_print(lst);
-    printf("Length = %d\n", pylist_len(lst));
-    printf("Brian? %d\n", pylist_index(lst, "Brian"));
-    printf("Bob? %d\n", pylist_index(lst, "Bob"));
+    // pylist_append(lst, "Catch phrase");
+    // pylist_print(lst);
+    // pylist_append(lst, "Brian");
+    // pylist_print(lst);
+    // printf("Length = %d\n", pylist_len(lst));
+    // printf("Brian? %d\n", pylist_index(lst, "Brian"));
+    // printf("Bob? %d\n", pylist_index(lst, "Bob"));
     pylist_del(lst);
 }
