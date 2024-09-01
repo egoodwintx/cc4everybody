339efb4e52ded9c7ee6d657991abc4ed87a74555
Ed Goodwin <ed.registration@gmail.com>
2024-09-01 18:23:34 -0500

    lesson10

---
+++ src/lesson10/iterator.c
@@ -0,0 +1,170 @@
+//---------------------------------------------------------
+// file: iterator.c
+// project: Learning C
+// author: Ed Goodwin
+// date:08.31.2024
+// description:
+//   OOP iterator
+//---------------------------------------------------------
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct MapEntry {
+    char *key;  /* public */
+    int value;  /* public */
+    struct MapEntry *__prev;
+    struct MapEntry *__next;
+};
+
+struct MapIter {
+   struct MapEntry *__current;
+
+   struct MapEntry* (*next)(struct MapIter* self);
+   void (*del)(struct MapIter* self);
+};
+
+struct Map {
+   /* Attributes */
+   struct MapEntry *__head;
+   struct MapEntry *__tail;
+   int __count;
+
+   /* Methods */
+   void (*put)(struct Map* self, char *key, int value);
+   int (*get)(struct Map* self, char *key, int def);
+   int (*size)(struct Map* self);
+   void (*dump)(struct Map* self);
+   struct MapIter* (*iter)(struct Map* self);
+   void (*del)(struct Map* self);
+};
+
+void __Map_del(struct Map* self) {
+    struct MapEntry *cur, *next;
+    cur = self->__head;
+    while(cur) {
+        free(cur->key);
+        /* value is just part of the struct */
+        next = cur->__next;
+        free(cur);
+        cur = next;
+    }
+    free((void *)self);
+}
+
+void __MapIter_del(struct MapIter* self) {
+    free((void *)self);
+}
+
+void __Map_dump(struct Map* self)
+{
+    struct MapEntry *cur;
+    printf("Object Map count=%d\n", self->__count);
+    for(cur = self->__head; cur != NULL ; cur = cur->__next ) {
+         printf("  %s=%d\n", cur->key, cur->value);
+    }
+}
+
+struct MapEntry* __Map_find(struct Map* self, char *key)
+{
+    struct MapEntry *cur;
+    if ( self == NULL || key == NULL ) return NULL;
+    for(cur = self->__head; cur != NULL ; cur = cur->__next ) {
+        if(strcmp(key, cur->key) == 0 ) return cur;
+    }
+    return NULL;
+}
+
+int __Map_get(struct Map* self, char *key, int def)
+{
+    struct MapEntry *retval = __Map_find(self, key);
+    if ( retval == NULL ) return def;
+    return retval->value;
+}
+
+int __Map_size(struct Map* self)
+{
+    return self->__count;
+}
+
+/* Student code will be inserted here */
+
+int main(void)
+{
+    struct Map * map = Map_new();
+    struct MapEntry *cur;
+    struct MapIter *iter;
+
+    /* Make sure we see all output up to an error */
+    setvbuf(stdout, NULL, _IONBF, 0); 
+
+    printf("Map test\n");
+    map->put(map, "z", 8);
+    map->put(map, "z", 1);
+    map->put(map, "y", 2);
+    map->put(map, "b", 3);
+    map->put(map, "a", 4);
+    map->dump(map);
+
+    printf("size=%d\n", map->size(map));
+
+    printf("z=%d\n", map->get(map, "z", 42));
+    printf("x=%d\n", map->get(map, "x", 42));
+
+    printf("\nIterate\n");
+    iter = map->iter(map);
+    while(1) {
+        cur = iter->next(iter);
+        if ( cur == NULL ) break;
+	printf("%s=%d\n", cur->key, cur->value);
+    }
+    iter->del(iter);
+
+    map->del(map);
+}
+
+void __Map_put(struct Map* self, char *key, int value) {
+    struct MapEntry *old, *new;
+    char *new_key;
+    if ( key == NULL ) return;
+
+    old = __Map_find(self, key);
+    if ( old != NULL ) {
+        old->value = value;
+        return;
+    }
+
+    new = malloc(sizeof(*new));
+
+    /* TODO: Link new to the tail of the list */
+
+    self->__count++;
+}
+
+struct MapEntry* __MapIter_next(struct MapIter* self)
+{
+    /* TODO: Advance the iterator */
+    return NULL;
+}
+
+struct MapIter* __Map_iter(struct Map* self)
+{
+    struct MapIter *iter = malloc(sizeof(*iter));
+    /* TODO: fill in the new iterator */
+    return iter;
+}
+
+struct Map * Map_new() {
+    struct Map *p = malloc(sizeof(*p));
+
+    p->__head = NULL;
+    p->__tail = NULL;
+    p->__count = 0;
+    p->put = &__Map_put;
+    p->get = &__Map_get;
+    p->size = &__Map_size;
+    p->dump = &__Map_dump;
+    p->del = &__Map_del;
+    return p;
+}
---
+++ src/lesson10/pythondict_class.c
@@ -0,0 +1,141 @@
+//---------------------------------------------------------
+// file: pythondict_class.c
+// project: Learning C
+// author: Ed Goodwin
+// date: 08.31.2024
+// description:
+//   redo of pythondict_class.c to enforce encapsulation
+//---------------------------------------------------------
+
+// EXPECTED OUTPUT
+// Map test
+// Object Map count=4
+//   z=1
+//   y=2
+//   b=3
+//   a=4
+// size=4
+// z=1
+// x=42
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct MapEntry {
+    char *key;  /* public */
+    int value;  /* public */
+    struct MapEntry *__prev;
+    struct MapEntry *__next;
+};
+
+struct Map {
+   /* Attributes */
+   struct MapEntry *__head;
+   struct MapEntry *__tail;
+   int __count;
+
+   /* Methods */
+   void (*put)(struct Map* self, char *key, int value);
+   int (*get)(struct Map* self, char *key, int def);
+   int (*size)(struct Map* self);
+   void (*dump)(struct Map* self);
+   void (*del)(struct Map* self);
+};
+
+void __Map_del(struct Map* self) {
+    struct MapEntry *cur, *next;
+    cur = self->__head;
+    while(cur) {
+        free(cur->key);
+        /* value is just part of the struct */
+        next = cur->__next;
+        free(cur);
+        cur = next;
+    }
+    free((void *)self);
+}
+
+void __Map_dump(struct Map* self)
+{
+    struct MapEntry *cur;
+    printf("Object Map count=%d\n", self->__count);
+    for(cur = self->__head; cur != NULL ; cur = cur->__next ) {
+         printf("  %s=%d\n", cur->key, cur->value);
+    }
+}
+
+struct MapEntry* __Map_find(struct Map* self, char *key)
+{
+    struct MapEntry *cur;
+    if ( self == NULL || key == NULL ) return NULL;
+    for(cur = self->__head; cur != NULL ; cur = cur->__next ) {
+        if(strcmp(key, cur->key) == 0 ) return cur;
+    }
+    return NULL;
+}
+
+/* Student code will be inserted here */
+
+int main(void)
+{
+    struct Map * map = Map_new();
+    struct MapEntry *cur;
+
+    /* Make sure we see all output up to an error */
+    setvbuf(stdout, NULL, _IONBF, 0); 
+
+    printf("Map test\n");
+    map->put(map, "z", 8);
+    map->put(map, "z", 1);
+    map->put(map, "y", 2);
+    map->put(map, "b", 3);
+    map->put(map, "a", 4);
+    map->dump(map);
+
+    printf("size=%d\n", map->size(map));
+
+    printf("z=%d\n", map->get(map, "z", 42));
+    printf("x=%d\n", map->get(map, "x", 42));
+
+    map->del(map);
+}
+
+
+void __Map_put(struct Map* self, char *key, int value) {
+    struct MapEntry *old, *new;
+    char *new_key;
+    if ( key == NULL ) return;
+
+    old = __Map_find(self, key);
+    if ( old != NULL ) {
+        old->value = value;
+        return;
+    }
+
+    new = malloc(sizeof(*new));
+
+    /* Need some code here... */
+
+    self->__count++;
+}
+
+int __Map_size(struct Map* self)
+{
+    return 42;
+}
+
+int __Map_get(struct Map* self, char *key, int def)
+{
+    /* You need some code here */
+    return def;
+}
+
+struct Map * Map_new() {
+    struct Map *p = malloc(sizeof(*p));
+
+    p->__head = NULL;
+    p->__tail = NULL;
+    p->__count = 0;
+    return p;
+}
