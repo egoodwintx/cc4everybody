6d72e25d7e2d3935edaaf46570f5868530ede427
Ed Goodwin <ed.registration@gmail.com>
2024-09-01 23:49:35 -0500

    finished exercise

--- src/lesson10/pythondict_class.c
+++ src/lesson10/pythondict_class.c
@@ -43,11 +43,12 @@ struct Map {
    void (*del)(struct Map* self);
 };
 
+// destructor
 void __Map_del(struct Map* self) {
     struct MapEntry *cur, *next;
     cur = self->__head;
     while(cur) {
-        free(cur->key);
+        // free(cur->key);
         /* value is just part of the struct */
         next = cur->__next;
         free(cur);
@@ -56,6 +57,7 @@ void __Map_del(struct Map* self) {
     free((void *)self);
 }
 
+// dumps the contents of Map to stdout
 void __Map_dump(struct Map* self)
 {
     struct MapEntry *cur;
@@ -70,12 +72,72 @@ struct MapEntry* __Map_find(struct Map* self, char *key)
     struct MapEntry *cur;
     if ( self == NULL || key == NULL ) return NULL;
     for(cur = self->__head; cur != NULL ; cur = cur->__next ) {
+        //printf("curkey val: %s %d\n", cur->key, cur->value);
         if(strcmp(key, cur->key) == 0 ) return cur;
     }
     return NULL;
 }
 
 /* Student code will be inserted here */
+
+void __Map_put(struct Map* self, char *key, int value) {
+    struct MapEntry *old, *new;
+    char *new_key;
+    if ( key == NULL ) return;
+
+    old = __Map_find(self, key);
+    if ( old != NULL ) {
+        old->value = value;
+        return;
+    }
+    else {
+        new = malloc(sizeof(*new));
+        new->key = key;
+        new->value = value;
+        new->__prev = self->__tail;
+        new->__next = NULL;
+        if (self->__head == NULL) {
+            self->__head = new;
+        }
+        if (self->__tail != NULL) {
+            (self->__tail)->__next = new;
+        }
+        self->__tail = new;
+        self->__count++;
+    }   
+}
+
+int __Map_size(struct Map* self)
+{
+    return self->__count;
+}
+
+int __Map_get(struct Map* self, char *key, int def)
+{
+    /* You need some code here */
+    struct MapEntry *retval = __Map_find(self, key);
+    if (retval == NULL){
+        return def;
+    }
+    return retval->value;
+}
+
+// completed
+struct Map * Map_new() {
+    struct Map *p = malloc(sizeof(*p));
+
+    p->__head = NULL;
+    p->__tail = NULL;
+    p->__count = 0;
+
+    p->put = &__Map_put;
+    p->get = &__Map_get;
+    p->size = &__Map_size;
+    p->dump = &__Map_dump;
+    p->del = &__Map_del;
+    return p;
+}
+
 
 int main(void)
 {
@@ -101,41 +163,3 @@ int main(void)
     map->del(map);
 }
 
-
-void __Map_put(struct Map* self, char *key, int value) {
-    struct MapEntry *old, *new;
-    char *new_key;
-    if ( key == NULL ) return;
-
-    old = __Map_find(self, key);
-    if ( old != NULL ) {
-        old->value = value;
-        return;
-    }
-
-    new = malloc(sizeof(*new));
-
-    /* Need some code here... */
-
-    self->__count++;
-}
-
-int __Map_size(struct Map* self)
-{
-    return 42;
-}
-
-int __Map_get(struct Map* self, char *key, int def)
-{
-    /* You need some code here */
-    return def;
-}
-
-struct Map * Map_new() {
-    struct Map *p = malloc(sizeof(*p));
-
-    p->__head = NULL;
-    p->__tail = NULL;
-    p->__count = 0;
-    return p;
-}
